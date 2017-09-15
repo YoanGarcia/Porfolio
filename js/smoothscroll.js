fb1d397ec3893eb782d87cb0f1f89a124bf6c176
AlexisMeunier <meunier_33@live.fr>
2017-09-15 14:46:38 +0200

    aze
    aze

--- js/smoothscroll.js
+++ js/smoothscroll.js
@@ -3,6 +3,8 @@ $(document).ready(function() {
 		var page = $(this).attr('href'); // Page cible
 		var speed = 750; // Durée de l'animation (en ms)
 		$('html, body').animate( { scrollTop: $(page).offset().top }, speed ); // Go
+		$('#sidemenu').remove();
+		$('#open_sidemenu').show();
 		return false;
 	});
 });
