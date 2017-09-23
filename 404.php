31a064b95ea008b5ec9b675bb3bc8d8b23838e95
YoanGarcia <yoan.gcia@hotmail.fr>
2017-09-23 16:19:32 +0200

    eza

---
+++ 404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initiam-scale-1.0" />
+
+	<title>Erreur 404</title>
+
+	<link rel="shortcut icon" type="image/jpg" href="IMG/MonLogoNoir.png" />
+	<link rel="stylesheet" type="text/css" href="css/404.css">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.min.css">
+</head>
+
+<body>
+	<section id="error404">
+		<br><br>
+		<h1>Erreur 404</h1>
+		<br><br><hr><br><br>
+		<a href="index.php" class="btna">Retour au site</a>
+		<br><br>
+	</section>
+</body>
+
+</html>
